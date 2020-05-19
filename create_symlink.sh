bf0ce23eba11f3021faf418958e010377a5526ac
Aidan Hammel <aidan@neptune.local>
2020-05-19 06:19:18 -0500

    Added install of zsh

--- create_symlink.sh
+++ create_symlink.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
+sudo apt update && sudo apt upgrade -y && sudo apt install zsh -y
 if test -f ~/.vimrc; then
     echo "Backing up .vimrc to .dotfiles/backups/"
     mv ~/.vimrc ~/.dotfiles/backups/
