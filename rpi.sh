ddab68a53329c1ecc1e65dcc1cf94fe4ffb1dece
hammela7 <hammela7@users.noreply.github.com>
2022-08-18 15:45:44 +0100

    Update rpi.sh

--- rpi.sh
+++ rpi.sh
@@ -2,5 +2,7 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt dist-upgrade -y
-sudo apt install git tmux vim htop curl
+sudo apt install git tmux vim htop curl -y
 curl -sSL https://get.docker.com | sh
+mkdir ~/d
+mkdir ~/d/syncthing
